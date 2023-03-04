6a4d266e9636e5ded1e27830098bcf5a0aefbf38
Didar Oreshev <doreshev@student.42heilbronn.de>
2023-03-04 18:39:33 +0100

    protection from null data

--- frontend/src/api/Data.js
+++ frontend/src/api/Data.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 
 function Data() {
-    const [data, getData] = useState([]);
+    const [data, setData] = useState(null);
     const city = '13.4050,52.5200';
     const baseurl = 'https://api.v2.emissions-api.org/api/v2/carbonmonoxide/statistics.json?interval=day&begin=2019-02-01&end=2019-02-02&point=';
 
@@ -10,20 +10,26 @@ function Data() {
         getAllData();
     }, []);
 
-    const getAllData = () => {
-        axios.get(baseurl + city)
-        .then((response) => {
+    const getAllData = async () => {
+        try {
+            const response = await axios.get(baseurl + city);
             const allData = response.data[0];
-            console.log();
-            getData(allData);
-        })
-        .catch(error => console.error('Error: ', error));
+            setData(allData);
+        }
+        catch(error) {
+            console.error('Error: ', error);
+        }
     }
+
     return (
         <div>
             <h1>My Data</h1>
-            <p>Average: {data.value.average}</p>
-            <p>Count: {data.value.count}</p>
+            {data && (
+                <div>
+                  <p>Average: {data.value.average}</p>
+                  <p>Count: {data.value.count}</p>
+                </div>
+            )}
         </div>
     );
 }
