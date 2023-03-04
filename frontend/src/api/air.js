70b671915e04911146a4a890c5348c6e41c3bcb7
Didar Oreshev <doreshev@student.42heilbronn.de>
2023-03-04 17:13:27 +0100

    fetch has been blocked by CORS....

--- frontend/src/api/air.js
+++ frontend/src/api/air.js
@@ -1,26 +1,46 @@
 import React, { useEffect, useState } from "react";
 
+// const URL = "https://httpbin.org/get"
+
 function Data() {
+
+    // const URL = 'https://www.umweltbundesamt.de/api/air_data/v2/component/json'
+    const URL = "https://httpbin.org/get"
     const [data, setData] = useState([]);
 
-  useEffect(() => {
-    const fetchData = async () => {
-      const response = await fetch(
-        "https://www.umweltbundesamt.de/api/air_data/v2/component/json" 
-      );
-      const result = await response.json();
-      setData(result);
-    };
-    fetchData();
-  }, []);
+    useEffect(() => {
+        async function getData() {
+            try {
+                const response = await fetch(URL);
+                if (!response.ok) {
+                    throw new Error(
+                      `This is an HTTP error: The status is ${response.status}`
+                    );
+                }
+                let result = await response.json();
+                console.log(result);
+                setData(result);
+            } 
+            catch(err) {
+                setData(null);
+            }
+        };
+        getData();
+    }, []);
 
-  return (
-    <div>
-      {data.map((item) => (
-        <p key={item.id}>{item.title}</p>
-      ))}
-    </div>
-  );
+    return (
+        <div className="Data">
+          <h1>API Posts</h1>
+          <ul>
+            {data &&
+              data.map(({ origin, url }) => (
+                <li key={origin}>
+                  <h3>{url}</h3>
+                </li>
+              ))}
+          </ul>
+        </div>
+      );
 }
 
 export default Data;
