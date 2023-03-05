eb3a95dfc666ffba06bc0fef44f03693a2169740
Didar Oreshev <doreshev@student.42heilbronn.de>
2023-03-05 14:37:07 +0100

    added charts

--- frontend/src/api/Data.js
+++ frontend/src/api/Data.js
@@ -1,8 +1,16 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
+import {createChart} from "./EmissionChart";
+import 'chart.js/auto';
+import { Chart } from 'react-chartjs-2';
 
-function Data(url) {
+
+function Data() {
     const [data, setData] = useState(null);
+    const location = '9.178666764773801,49.11866136687143'
+    const baseurl = 
+    'https://api.v2.emissions-api.org/api/v2/carbonmonoxide/statistics.json?interval=day&begin=2022-01-01&end=2022-07-30&point=';
+  
 
     useEffect(() => {
         getAllData();
@@ -10,9 +18,8 @@ function Data(url) {
 
     const getAllData = async () => {
         try {
-            const response = await axios.get(url);
-            const allData = response.data[0];
-            console.log(allData);
+            const response = await axios.get(baseurl + location);
+            const allData = response.data;
             setData(allData);
         }
         catch(error) {
@@ -22,14 +29,15 @@ function Data(url) {
 
     return (
         <div>
-            <h1>My Data</h1>
-            {data && (
-                <div>
-                  <p>Average: {data.value.average}</p>
-                  <p>Count: {data.value.count}</p>
-                </div>
-            )}
+            <div>
+                <h1>My Data</h1>
+                {data && <Chart type='bar' data={createChart(data)} />}
+            </div>
+            <div>
+                
+            </div>
         </div>
+
     );
 }
 
---
+++ frontend/src/api/EmissionChart.js
@@ -0,0 +1,33 @@
+function createChart(emissionData) {
+  return {
+    // labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+    labels: [
+      emissionData[0].time.interval_start.slice(0, 7),
+      emissionData[10].time.interval_start.slice(0, 7),
+      emissionData[1].time.interval_start.slice(0, 7),
+      emissionData[4].time.interval_start.slice(0, 7),
+      emissionData[3].time.interval_start.slice(0, 7),
+      emissionData[26].time.interval_start.slice(0, 7),
+      emissionData[11].time.interval_start.slice(0, 7),
+    ],
+    datasets: [
+      {
+        label: '2022 Heilbronn corbonate emission',
+        backgroundColor: 'rgba(75,192,192,1)',
+        borderColor: 'rgba(0,0,0,1)',
+        borderWidth: 2,
+        data: [
+         emissionData[0].value.average,
+         emissionData[10].value.average,
+         emissionData[1].value.average,
+         emissionData[4].value.average,
+         emissionData[3].value.average,
+         emissionData[26].value.average,
+         emissionData[11].value.average
+        ]
+      }
+    ]
+  }
+}
+
+export {createChart};
